e99e8b84339f8acbfeb9e09fb5c1b3c8ec4b9080
CFCode-git <435184766@qq.com>
2020-11-25 17:08:32 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+// https://jsbin.com/lesivij/1/edit?html,js
+const express = require('express')
+const multer = require('multer') // 获取用户上传的文件
+const cors = require('cors') // 跨域
+
+const upload = multer({ dest: 'uploads/' }) // 上传后保存的路径
+const app = express()
+
+app.get('/', (request, response, next) => {
+  response.send('hello,express!')
+})
+
+app.options('/upload', cors())
+app.post('/upload', cors(), upload.single('file'), (request, response, next) => {
+  // response.set('Access-Control-Allow-Origin', '*')
+  response.send(request.file.filename)
+})
+
+app.get('/preview/:key', cors(), (request, response, next) => {
+  response.sendFile(`uploads/${request.params.key}`, {
+    root: __dirname,
+    headers: {
+      'Content-Type': 'image/jpeg'
+    }
+  },
+    (error) => {
+      // response.status(404).send('file not find')
+      console.log(error)
+    })
+})
+
+app.listen(8888, () => {
+  console.log('now you listening the port 8888!')
+})
